9a785822afacf7106c3358c2655d42db210c2a2d
ppolcz <ppolcz@gmail.com>
2016-08-01 19:33:19 +0200

    com.oakdalesoft.bootfaces.domain.Book -- EZZEL MEG BAJ VANgit status!

--- java-spring-boot-mysql-jpa-hibernate/src/main/java/com/oakdalesoft/bootfaces/Application.java
+++ java-spring-boot-mysql-jpa-hibernate/src/main/java/com/oakdalesoft/bootfaces/Application.java
@@ -1,8 +1,7 @@
-package polcz.budget;
+package com.oakdalesoft.bootfaces;
 
 import java.util.EnumSet;
-import javax.faces.webapp.FacesServlet;
-import javax.servlet.DispatcherType;
+
 import org.ocpsoft.rewrite.servlet.RewriteFilter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
@@ -18,13 +17,19 @@ import org.springframework.core.io.PathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.data.repository.init.Jackson2RepositoryPopulatorFactoryBean;
 
-//@SpringBootApplication
-@EnableAutoConfiguration
-@ComponentScan({"polcz.budget"})
-public class Application extends SpringBootServletInitializer {
+import javax.faces.webapp.FacesServlet;
+import javax.servlet.DispatcherType;
 
-    @Value("${init.json}")
-    private String init;
+/**
+ * Created by Alex on 28/02/2015.
+ */
+
+@EnableAutoConfiguration
+@ComponentScan({"com.oakdalesoft.bootfaces"})
+public class Application extends SpringBootServletInitializer {
+	
+	@Value("${init.json}")
+	private String init;
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
@@ -41,7 +46,7 @@ public class Application extends SpringBootServletInitializer {
         ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(servlet, "*.jsf");
         return servletRegistrationBean;
     }
-
+    
     @Bean
     public FilterRegistrationBean rewriteFilter() {
         FilterRegistrationBean rwFilter = new FilterRegistrationBean(new RewriteFilter());
@@ -50,23 +55,22 @@ public class Application extends SpringBootServletInitializer {
         rwFilter.addUrlPatterns("/*");
         return rwFilter;
     }
-
+    
     @Bean
     public Jackson2RepositoryPopulatorFactoryBean repositoryPopulator() {
-        Resource sourceData;
-        Jackson2RepositoryPopulatorFactoryBean factory;
-        try {
-            sourceData = new PathResource(init);
-            if (!sourceData.exists()) {
-                sourceData = new ClassPathResource(init);
-            }
-            factory = new Jackson2RepositoryPopulatorFactoryBean();
-            factory.setResources(new Resource[]{sourceData});
-        } catch (Exception e) {
-            return null;
-        }
+    	Resource sourceData;
+    	Jackson2RepositoryPopulatorFactoryBean factory;
+		try {
+			sourceData = new PathResource(init);
+			if(!sourceData.exists())
+				sourceData = new ClassPathResource(init);
+			factory = new Jackson2RepositoryPopulatorFactoryBean();
+			factory.setResources(new Resource[] { sourceData });
+		} catch (Exception e) {
+			return null;
+		}
 
-        return factory;
+		return factory;
     }
 
 }
